ab6e19736cde9e68364cf546b004f4be83591f4c
SOL <75055393+ma0625@users.noreply.github.com>
2021-09-13 14:08:28 +0900

    Delete PropTechAPI - コピー directory

--- PropTechAPI - コピー/src/main/java/com/PropTech/UserRegist/PropTechAppAPI/PropTechAPI/Config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.PropTech.UserRegist.PropTechAppAPI.PropTechAPI.Config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class ApplicationConfig extends Application{
-
-}
