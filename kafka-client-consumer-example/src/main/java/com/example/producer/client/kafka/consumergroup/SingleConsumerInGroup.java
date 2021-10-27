410026b6a33bc7d6764be937bc9c2e419e92535d
SOL <75055393+ma0625@users.noreply.github.com>
2021-10-27 14:30:11 +0900

    Delete kafka-client-consumer-example directory

--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumergroup/SingleConsumerInGroup.java
+++
@@ -1,8 +0,0 @@
-package com.example.producer.client.kafka.consumergroup;
-
-public class SingleConsumerInGroup {
-    public static void main(String[] args) {
-        ConsumeTask consumeTask = new ConsumeTask("myConsumer", "sin");
-        new Thread(consumeTask).start();
-    }
-}
