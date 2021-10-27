97dc47bdfa89670e718662ce872425c7514a97d2
SOL <75055393+ma0625@users.noreply.github.com>
2021-10-27 13:41:21 +0900

    Delete async directory

--- async/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.open.jp.async.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class ApplicationConfig extends Application{
-}
-
